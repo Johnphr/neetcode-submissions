fd66b18e9e47bff2b14a6116dd1a87c94123425b
Johnphr <131493470+Johnphr@users.noreply.github.com>
2026-08-27 11:54:26 -0600

    Add: decode-ways - submission-5

---
+++ Data Structures & Algorithms/decode-ways/submission-5.py
@@ -0,0 +1,18 @@
+class Solution:
+    def numDecodings(self, s: str) -> int:
+        memo = {}
+        invalid = set(['7', '8', '9'])
+        starts = set(['1', '2'])
+        def dp(s):
+            if s in memo:
+                return memo[s]
+            if len(s) > 0 and s[0] == '0':
+                return 0
+            if len(s) <= 1:
+                return 1
+            if len(s) > 1 and ((s[0] == '1') or (s[0] == '2' and s[1] not in invalid)):
+                memo[s] = dp(s[1:len(s)]) + dp(s[2:len(s)])
+            else:
+                memo[s] = dp(s[1:len(s)])
+            return memo[s]
+        return dp(s)
