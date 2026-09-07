f6abf4ff043b49f0b53ff951db93ff3591f38961
Johnphr <131493470+Johnphr@users.noreply.github.com>
2026-09-07 10:53:57 -0600

    Add: course-schedule - submission-10

---
+++ Data Structures & Algorithms/course-schedule/submission-10.py
@@ -0,0 +1,29 @@
+from collections import deque
+class Solution:
+    def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
+        myMap = {}
+        inEdges = [0] * (numCourses + 1)
+        for i in range(numCourses):
+            myMap[i] = []
+            inEdges[i] = 0
+
+        for a, b in prerequisites:
+            myMap[b].append(a)
+            inEdges[a] += 1
+        q = deque()
+        for i in range(numCourses):
+            if inEdges[i] == 0:
+                q.append(i)
+        res = 0
+        while q:
+            node = q.popleft()
+            res += 1
+            for conn in myMap[node]:
+                inEdges[conn] -= 1
+                if inEdges[conn] == 0:
+                    q.append(conn)
+        if res < numCourses:
+            return False
+        return True
+
+        
