529042b1a7d40e2342e3c05b93bb71e27eeb2214
Johnphr <131493470+Johnphr@users.noreply.github.com>
2026-09-19 20:03:22 -0600

    Add: reconstruct-flight-path - submission-4

---
+++ Data Structures & Algorithms/reconstruct-flight-path/submission-4.py
@@ -0,0 +1,23 @@
+class Solution:
+    def findItinerary(self, tickets: List[List[str]]) -> List[str]:
+        adj = {}
+        t = len(tickets)
+        for u, v in tickets:
+            if u not in adj:
+                adj[u] = []
+            if v not in adj:
+                adj[v] = []
+        for src, dst in sorted(tickets)[::-1]:
+            adj[src].append(dst)
+
+        stack = ["JFK"]
+        res = []
+
+        while stack:
+            curr = stack[-1]
+            if not adj[curr]:
+                res.append(stack.pop())
+            else:
+                stack.append(adj[curr].pop())
+
+        return res[::-1]
